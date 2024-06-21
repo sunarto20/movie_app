0f6cf3fed33474e719311d3df7b0435976b623cf
Sunarto <sunarto.narnia@gmail.com>
2024-06-21 20:07:13 +0700

    add prettier and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    extends: [
+      'plugin:react/recommended',
+      'standard-with-typescript',
+      'prettier', // Ditambahkan
+    ],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        project: './tsconfig.json',
+      },
+    },
+  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+]
